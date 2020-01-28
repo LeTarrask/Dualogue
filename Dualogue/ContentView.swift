b65b63ab4254d0c680a9c988c89f5a50e5ddc16d
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-01-28 12:20:15 +0000

    split the views in different files added newnoteview

--- Dualogue/ContentView.swift
+++ Dualogue/ContentView.swift
@@ -44,56 +44,6 @@ struct ContentView: View {
     }
 }
 
-struct MainView: View {
-    var data = NoteBuilderTest()
-    
-    var body: some View {
-        VStack {
-            VStack {
-                Text("Calendar Button")
-                Text("Banner")
-            }
-            HStack {
-                Text("Recent posts")
-                Spacer()
-                Text("Jan 2017")
-            }
-            ScrollView(.vertical, content: {
-                HStack(spacing: 10) {
-                    ForEach(data.noteCollection) { note in
-                        NotePreview(note: note)
-                    }
-                }
-                .padding(.leading, 10)
-            })
-                .frame(height: 190)
-        }
-    }
-}
-
-struct NotePreview: View {
-    let note: DuaNote
-    
-    var body: some View {
-        
-        VStack(alignment: .leading) {
-            HStack {
-                AvatarView(image: note.user.image, size: 48)
-                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 0))
-                Text(note.user.name)
-                    .font(.caption)
-            }
-            Text(note.text)
-                .font(.headline)
-                .lineLimit(4)
-                .padding(10)
-        }
-        .background(Color.gray)
-        .frame(width: 200).cornerRadius(20)
-    }
-}
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
