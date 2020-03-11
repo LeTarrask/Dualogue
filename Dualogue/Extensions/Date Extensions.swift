e3082ce17b5a81c92267de841e0805e4e67c5fc3
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-03-11 12:04:51 +0000

    moved date extension helper to extensions group

---
+++ Dualogue/Extensions/Date Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Date Extensions.swift
+//  Dualogue
+//
+//  Created by Alex Luna on 11/03/2020.
+//  Copyright © 2020 Garagem Infinita. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func toString(dateFormat format: String ) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        //        dateFormatter.dateStyle = .full
+        return dateFormatter.string(from: self)
+    }
+}
