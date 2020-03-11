075aeac3a22610a1a52b7800202c55fa888f0c21
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-03-11 13:07:47 +0000

    removed duplicate extension to date

--- Dualogue/View/Timelines/NoteTimeline.swift
+++ Dualogue/View/Timelines/NoteTimeline.swift
@@ -47,12 +47,3 @@ struct NoteTimeline_Previews: PreviewProvider {
         NoteTimeline()
     }
 }
-
-extension Date {
-    func toString(format: String = "yyyy-MM-dd") -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.dateFormat = format
-        return formatter.string(from: self)
-    }
-}
