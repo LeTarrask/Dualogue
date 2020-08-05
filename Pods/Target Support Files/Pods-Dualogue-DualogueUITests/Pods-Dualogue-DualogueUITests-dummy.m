8880ee62a9322e8357402de51c338b7ebeeeb27e
Alex Luna <14889546+LeTarrask@users.noreply.github.com>
2020-08-05 11:34:36 +0100

    Merge after tests

---
+++ Pods/Target Support Files/Pods-Dualogue-DualogueUITests/Pods-Dualogue-DualogueUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dualogue_DualogueUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dualogue_DualogueUITests
+@end
---
+++ Pods/Target Support Files/Pods-DualogueTests/Pods-DualogueTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DualogueTests : NSObject
+@end
+@implementation PodsDummy_Pods_DualogueTests
+@end
